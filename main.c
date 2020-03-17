e84171bdc0e0cc9c67bf1984271a6b53ffa6dad3
Edgar Mancilla <delkoman@gmail.com>
2020-03-17 09:34:27 -0700

    First commit

--- main.c
+++ main.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-//Función para elevar x numero a y potencia
-float potencia(int a, int b)
+//Función para cambiar de pesos a dólares
+float dolares(float a, float b)
 {
-    return pow(a, b);
+    return a * b;
 
+}
+
+float pesos(float b, float c)
+{
+    return b * c;
 }
 
 int main()
 {
+    int tipocambio;
+
+    printf("Si quieres cambiar de dolares a pesos escribe 1, si quieres cambiar de pesos a dolares escribe 2: ")
+    scanf("%i", &tipocambio);
+
 //se declaran las variables
-    float a;
-    float b;
-    printf("Elevar un numero a x potencia:\n");
+        float a = 0.043;
+        float b;
+    printf("Convertidor de pesos a dolares:\n");
 //Se pide el núimero base
-    printf("\n Ingresa el numero base:");
-    scanf("%f", &a);
-//Se pide la potencia
-    printf("\n Ingresa el exponente:");
+    printf("\n Ingresa el valor en pesos: ");
     scanf("%f", &b);
+
 //Se guarda el resultado
-    float potenciaRes = potencia(a,b);
+    float dolaresRes = dolares(a,b);
 //Se imprime el resultado
-    printf("\n El resultado es: %f \n", potenciaRes);
+    printf("\n %f pesos son: %f dolares \n", b, dolaresRes);
 
 
     return 0;
